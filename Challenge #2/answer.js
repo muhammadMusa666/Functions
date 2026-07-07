cebecf28d5de82f9e3c916bafda67e68b0e23718
muhammadMusa666 <Unkox1890@gmail.com>
2026-07-07 22:40:53 +0500

    Made With Love By Musa!

---
+++ Challenge #2/answer.js
@@ -0,0 +1,12 @@
+// Hopefully You Completed This Challenge, It Was Pretty Easy Compared To The Others,
+// And Even If You Couldnt Do It, At Least You Tried, I Will Hint,
+// It Was About Closures, But Anyway, Here's Your Code Answer,
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'white'
+
+  document.querySelector('body').addEventListener('click', function() {
+    header.style.color = 'blue';
+  });
+})();
+// I Really Wish Your Learning Something, And Im Sorry If These Seem Half-Baked😔,
