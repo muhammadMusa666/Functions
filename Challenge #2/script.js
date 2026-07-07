c2fbd3c492e9aa6ed12fb176a6f35e7addf9e00c
muhammadMusa666 <Unkox1890@gmail.com>
2026-07-07 22:40:43 +0500

    Made With Love By Musa!

---
+++ Challenge #2/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Today, We Will Be Doing Our 2nd And Final Challenge Of This Section,
+// Infact, This Is Is The Last Subrepo Of This Section,
+// So, Lettuce Begin🥬!
+
+// This Is More Of A Thinking Challenge Than A Coding Challenge,
+// Which Is Why It Will Be Short,
+
+// Take The IIFE Below And At The End Of The Function, Attach An eventListener,
+// That Changes The Color Of The Selected h1 Element('header') To Blue,
+// Each Time The BODY Element Is Clicked, Do NOT Select The h1 Element Again!
+
+// And Now Explain To YOURSELF (Or Someone Around You), Why This Worked,
+// Take All The Time You Need, Think About WHEN Exactly The Callback Function,
+// Is Executed, And What That Means For The Variables Involved In This Example,
+// (I Wont Answer This Part, Since I Want You To Ask AIs And Find It Yourself)
+// Good Luck😄!
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'white'
+})();
+
+
+
+
+
+
+
+
+
+
